39be36b460ccc9b52aedb69083be4ba3b9b434a7
ramenumai <taiki1151001@icloud.com>
2023-02-14 16:37:03 +0900

    ナビゲーションバーにissue項目を追加

--- resources/views/issue/index.blade.php
+++ resources/views/issue/index.blade.php
@@ -1,6 +1,13 @@
-@foreach($titles as $title)
-<p>{{$title}}</p>
-@endforeach
-@foreach($url as $url)
-<p>{{$url}}</p>
-@endforeach
+
+
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,13 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <!-- Issue Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('issue.index')" :active="request()->routeIs('issue.index')">
+                        {{ __('Issue List') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
