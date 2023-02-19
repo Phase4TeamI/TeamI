33b698282c562dd4cf766edb1816d64c2cd5a248
ramenumai <taiki1151001@icloud.com>
2023-02-19 16:55:56 +0900

    DETAILS部分にアイコンを追加

--- resources/views/scoreboard/index.blade.php
+++ resources/views/scoreboard/index.blade.php
@@ -6,29 +6,46 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="grid grid-cols-2 gap-5 p-6">
+                <div class="grid grid-cols-5 gap-5 p-6">
 
                   <!-- スコア -->
-                  <div class="dark:text-white">
-                    <div class="w-full border px-3 py-6 rounded-xl">
+                  <div class="dark:text-white col-start-1 col-span-2">
+                    <div class="w-full border px-3 py-7 rounded-xl">
                       <h3 class="font-mono text-xl">SCORE</h3>
                       <p class="font-mono text-5xl mt-3">3356</p>
                     </div>
                   </div>
 
                   <!-- グラフ -->
-                  <div class="dark:text-white flex items-center justify-center px-3 py-6 row-span-2 text-center border rounded-xl">グラフがきます</div>
+                  <div class="dark:text-white flex items-center justify-center px-3 py-7 row-span-2 col-start-3 col-span-3 text-center border rounded-xl">グラフがきます</div>
 
                   <!-- 詳細 -->
-                  <div class="dark:text-white">
-                    <div class="w-full border px-3 py-6 rounded-xl">
-                      <h3 class="font-mono text-xl">DETAILS</h3>
-                      <p class="font-mono text-lg mt-3 uppercase">issues</p>
-                      <p class="font-mono text-lg mt-3 uppercase">pull requests</p>
-                      <p class="font-mono text-lg mt-3 uppercase">commits</p>
-                      <p class="font-mono text-lg mt-3 uppercase">member</p>
+                  <div class="dark:text-white col-start-1 col-span-2">
+                    <div class="w-full border px-3 py-7 rounded-xl">
+                      <h3 class="font-mono text-2xl">DETAILS</h3>
+
+                      <div class="flex mt-5 items-center">
+                        <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r=".5" fill="currentColor" />  <circle cx="12" cy="12" r="9" /></svg>
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                      </div>
+
+                      <div class="flex mt-5 items-center">
+                        <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="6" cy="18" r="2" />  <circle cx="6" cy="6" r="2" />  <circle cx="18" cy="18" r="2" />  <line x1="6" y1="8" x2="6" y2="16" />  <path d="M11 6h5a2 2 0 0 1 2 2v8" />  <polyline points="14 9 11 6 14 3" /></svg>
+                        <p class="font-mono text-lg uppercase">　pull requests</p>
+                      </div>
+
+                      <div class="flex mt-5 items-center">
+                        <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r=".5" fill="currentColor" />  <circle cx="12" cy="12" r="9" /></svg>
+                        <p class="font-mono text-lg uppercase">　commits</p>
+                      </div>
+
+                      <div class="flex mt-5 items-center">
+                        <svg class="h-5 w-5 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
+                        <p class="font-mono text-lg uppercase">　member</p>
+                      </div>
+                      
                     </div>
                   </div>
 
