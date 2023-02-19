01dbff5195501bcd4c22d77225eb24d5fedb2493
ramenumai <taiki1151001@icloud.com>
2023-02-19 14:34:45 +0900

    ナビゲーションバーにscoreboardを追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,6 +23,13 @@
                         {{ __('Issue List') }}
                     </x-nav-link>
                 </div>
+
+                <!-- Scoreboard Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('scoreboard.index')" :active="request()->routeIs('scoreboard.index')">
+                        {{ __('Scoreboard') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
