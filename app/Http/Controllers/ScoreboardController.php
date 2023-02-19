0ebc1d3b2b939097a1f83509de34bf8904e4defe
ramenumai <taiki1151001@icloud.com>
2023-02-19 17:27:49 +0900

    ScoreboardControllerにjsonを読み込む処理を記述

--- app/Http/Controllers/ScoreboardController.php
+++ app/Http/Controllers/ScoreboardController.php
@@ -13,8 +13,30 @@ class ScoreboardController extends Controller
      */
     public function index()
     {
-        return view('scoreboard.index');
-    }
+        //User_Agentの権限を許可する
+        $ctx = stream_context_create(array(
+            'http' => array(
+                'method' => 'GET',
+                'header' => 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko')
+            )
+        );
+
+        //JSONデータが置かれているURL先を格納する
+        $issue_url = "https://api.github.com/repos/phase4TeamI/TeamI/issues";
+
+        //JSONデータを全て文字列に読み込むためにjsonという変数を作製
+        $json = file_get_contents($issue_url, false, $ctx);
+
+        //文字化け対策
+        $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        
+        //第二引数にtrueを使用することで連想配列にすることができる
+        $ary = json_decode($json,true);
+
+        
+        
+        
+        }
 
     /**
      * Show the form for creating a new resource.
