8bbe4b69cf8b57a485f3041359db6cba00bd4f45
ramenumai <taiki1151001@icloud.com>
2023-02-22 21:32:24 +0900

    scoreboardのissue周りの実装

--- app/Http/Controllers/ScoreboardController.php
+++ app/Http/Controllers/ScoreboardController.php
@@ -25,46 +25,89 @@ class ScoreboardController extends Controller
 
         //issueのURL
         $issue_url = "https://api.github.com/repos/phase4TeamI/TeamI/issues?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
-        //pull requestのURL
+        $issue_close_url = "https://api.github.com/repos/phase4TeamI/TeamI/issues?state=closed";
+
+        // pull requestのURL
         $pull_url = "https://api.github.com/repos/phase4TeamI/TeamI/pulls?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
-        //commit
-        $commit_url = "https://api.github.com/repos/phase4TeamI/TeamI/commits?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
-        //members
-        $member_url = "https://api.github.com/orgs/Phase4TeamI/members?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
+        $close_pull_url = "https://api.github.com/repos/phase4TeamI/TeamI/pulls?state=close?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
+
+        // //commit
+        // $commit_url = "https://api.github.com/repos/phase4TeamI/TeamI/commits?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
+        // //members
+        // $member_url = "https://api.github.com/orgs/Phase4TeamI/members?client_id=df3c312a6607af1baa5b&client_secret=5a4d5a8dc7ba604ee3b965d18abe347741c05c09";
 
         //JSONデータを全て文字列に読み込むためにjsonという変数を作製
         $issue_json = file_get_contents($issue_url, false, $ctx);
+        $issue_close_json = file_get_contents($issue_close_url, false, $ctx);
+
         $pull_json = file_get_contents($pull_url, false, $ctx);
-        $commit_json = file_get_contents($commit_url, false, $ctx);
-        $member_json = file_get_contents($member_url, false, $ctx);
+        $close_pull_json = file_get_contents($close_pull_url, false, $ctx);
+        // $commit_json = file_get_contents($commit_url, false, $ctx);
+        // $member_json = file_get_contents($member_url, false, $ctx);
 
         //文字化け対策
         $issue_json = mb_convert_encoding($issue_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        $issue_close_json = mb_convert_encoding($issue_close_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+
         $pull_json = mb_convert_encoding($pull_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-        $commit_json = mb_convert_encoding($commit_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-        $member_json = mb_convert_encoding($member_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        $close_pull_json = mb_convert_encoding($close_pull_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        // $commit_json = mb_convert_encoding($commit_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        // $member_json = mb_convert_encoding($member_json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
         
         //第二引数にtrueを使用することで連想配列にすることができる
         $issue_ary = json_decode($issue_json,true);
+        $issue_close_ary = json_decode($issue_close_json,true);
+
         $pull_ary = json_decode($pull_json,true);
-        $commit_ary = json_decode($commit_json,true);
-        $member_ary = json_decode($member_json,true);
+        $close_pull_ary = json_decode($close_pull_json,true);
+        // $commit_ary = json_decode($commit_json,true);
+        // $member_ary = json_decode($member_json,true);
 
-        $issue_json_count = count($issue_ary);
-        $pull_json_count = count($pull_ary);
-        $commit_json_count = count($commit_ary);
-        $member_json_count = count($member_ary);
         
-        // ddd($member_json_count);
-        $results[] = array(
-            'issue'  => $issue_json_count,
-            'pull' => $pull_json_count,
-            'commit'  => $commit_json_count,
-            'member' => $member_json_count
-        );
-        // ddd($results);
+        $issue_count = count($issue_ary);
+        $issue_close_count = count($issue_close_ary);
+        
+        $pull_count = count($pull_ary);
+        $close_pull_count = count($close_pull_ary);
 
-        return view('scoreboard.index', compact('results'));
+        // $commit_json_count = count($commit_ary);
+        // $member_json_count = count($member_ary);
+
+        if ($issue_close_ary === NULL){
+            return;
+        }else {
+
+            //クローズissueにプルリクエストが含まれているのでプルリクを除く処理
+            $new_close_issue = array();
+
+            for($i = 0; $i <= $issue_close_count-1; $i++){
+                //pullが含まれる場合はスキップする
+                if(strpos($issue_close_ary[$i]["html_url"], 'https://github.com/Phase4TeamI/TeamI/pull/') !== false){
+                    
+                }
+                else{
+                    $new_close_issue[] = $issue_close_ary[$i];
+                }
+            }
+
+            for($i = 0; $i <= count($new_close_issue); $i++){
+                $issue_open[] = new Carbon($new_close_ary[$i]["created_at"]);
+            }
+
+            ddd($new_close_issue);
+
+            $issues[] = array(
+                'open'  => $issue_count-$pull_count,
+                'close' => $close_issue_count-$close_pull_count,
+                // 'pull' => $pull_json_count,
+                // 'commit'  => $commit_json_count,
+                // 'member' => $member_json_count
+            );
+
+        return view('scoreboard.index', compact('issues'));
+        }
+
+        
     }
 
     
--- resources/views/scoreboard/index.blade.php
+++ resources/views/scoreboard/index.blade.php
@@ -7,11 +7,11 @@
 
     <div class="py-12">
         <div class="w-full mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden  sm:rounded-lg">
                 <div class="grid grid-cols-5 gap-5 p-6">
 
                   <!-- スコア -->
-                  <div class="dark:text-white col-start-1 col-span-2">
+                  <div class="dark:text-white col-start-1 bg-gray-800 rounded-xl shadow-lg col-span-2 ">
                     <div class="w-full border px-3 py-7 rounded-xl">
                       <h3 class="font-mono text-xl">SCORE</h3>
                       <p class="font-mono text-5xl mt-3">3356</p>
@@ -26,18 +26,37 @@
                     <div class="w-full border px-3 py-7 rounded-xl">
                       <h3 class="font-mono text-2xl">DETAILS</h3>
 
-                      <div class="flex mt-5 items-center justify-between">
-
-                        <div class="flex items-center ac-parent">
+                      <!-- issue情報 -->
+                      <div class="flex mt-5 items-center justify-between ac-parent">
+                        <div class="flex items-center ">
                           <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r=".5" fill="currentColor" />  <circle cx="12" cy="12" r="9" /></svg>
                           <p class="font-mono text-lg uppercase">　issues</p>
                         </div>
 
-                        <!-- <div class="">
+                        <div class="">
                           <p class="font-mono text-lg">{{$results[0]["issue"]}}</p>
-                        </div> -->
-
+                        </div>
                       </div>
+
+                      <div class="ml-5 space-y-2 ac-child">
+                        <p class="font-mono text-lg mt-2">　オープンしているissues</p>
+                        <p class="font-mono text-lg ">　クローズしたissues</p>
+                        <p class="font-mono text-lg ">　1issueあたりの平均クローズ時間</p>
+                        <p class="font-mono text-lg ">　issues</p>
+                      </div>
+
+                      <!-- プルリク情報 -->
+                      <div class="flex mt-5 items-center justify-between ac-parent">
+                        <div class="flex items-center ">
+                          <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="6" cy="18" r="2" />  <circle cx="6" cy="6" r="2" />  <circle cx="18" cy="18" r="2" />  <line x1="6" y1="8" x2="6" y2="16" />  <path d="M11 6h5a2 2 0 0 1 2 2v8" />  <polyline points="14 9 11 6 14 3" /></svg>
+                          <p class="font-mono text-lg uppercase">　pull requests</p>
+                        </div>
+
+                        <div>
+                          <p class="font-mono text-lg"></p>
+                        </div>
+                      </div>
+
                       <div class="ml-5 ac-child">
                         <p class="font-mono text-lg uppercase">　issues</p>
                         <p class="font-mono text-lg uppercase">　issues</p>
@@ -45,42 +64,42 @@
                         <p class="font-mono text-lg uppercase">　issues</p>
                       </div>
 
-                      <div class="flex mt-5 items-center justify-between">
-
-                        <div class="flex items-center ">
-                          <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="6" cy="18" r="2" />  <circle cx="6" cy="6" r="2" />  <circle cx="18" cy="18" r="2" />  <line x1="6" y1="8" x2="6" y2="16" />  <path d="M11 6h5a2 2 0 0 1 2 2v8" />  <polyline points="14 9 11 6 14 3" /></svg>
-                          <p class="font-mono text-lg uppercase">　pull requests</p>
-                        </div>
-
-                        <div>
-                          <p class="font-mono text-lg">{{$results[0]["pull"]}}</p>
-                        </div>
-
-                      </div>
-
-                      <div class="flex mt-5 items-center justify-between">
-
+                      <!-- コミット情報 -->
+                      <div class="flex mt-5 items-center justify-between ac-parent">
                         <div class="flex items-center">
                           <svg class="h-5 w-5 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r=".5" fill="currentColor" />  <circle cx="12" cy="12" r="9" /></svg>
                           <p class="font-mono text-lg uppercase">　commits</p>
                         </div>
 
                         <div>
-                          <p class="font-mono text-lg">{{$results[0]["commit"]}}</p>
+                          <p class="font-mono text-lg"></p>
                         </div>
-
                       </div>
 
-                      <div class="flex mt-5 items-center justify-between">
+                      <div class="ml-5 ac-child">
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                      </div>
 
+                      <!-- メンバー情報 -->
+                      <div class="flex mt-5 items-center justify-between ac-parent">
                         <div class="flex items-center">
                           <svg class="h-5 w-5 text-gray-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
                           <p class="font-mono text-lg uppercase">　member</p>
                         </div>
 
                         <div>
-                          <p class="font-mono text-lg">{{$results[0]["member"]}}</p>
+                          <p class="font-mono text-lg"></p>
                         </div>
+                      </div>
+
+                      <div class="ml-5 ac-child">
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                        <p class="font-mono text-lg uppercase">　issues</p>
+                        <p class="font-mono text-lg uppercase">　issues</p>
                       </div>
                       
                     </div>
