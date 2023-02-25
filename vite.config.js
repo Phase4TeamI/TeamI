9d160852ef6606e79f70918b6ed583ffce3fa2c4
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-02-25 16:47:57 +0900

    vite.configを修正

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/index.js',
+                'resources/css/index.css',
             ],
             refresh: true,
         }),
