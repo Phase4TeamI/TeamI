dfdaa554e84b7e97b5906b487f1ea74eb21e9a44
ku6kaw <reoreo01.123@gmail.com>
2023-02-26 16:30:16 +0900

    repository/createのスタイルを調整

--- resources/views/repository/create.blade.php
+++ resources/views/repository/create.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Register Repository') }}
+        </h2>
+    </x-slot>
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-4">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                        <h2 class="mx-auto text-center text-xl font-medium text-gray-900 dark:text-gray-100">
                             {{ __('リポジトリの登録') }}
                         </h2>
-                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                            {{ __("既存のGitHubリポジトリを登録します") }}
+                        <p class="mx-auto text-center mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            {{ __("既存のGitHubリポジトリを登録できます") }}
                         </p>
                         <form action="{{route('repository.store')}}" method="post" class="mt-6 space-y-6">
                             @csrf
@@ -24,7 +30,9 @@
                                 <x-input-label for="webhook_secret" :value="__('Webhook Secret')" />
                                 <x-text-input name="webhook_secret" type="text" class="mt-1 block w-full"/>
                             </div>
-                            <x-primary-button>{{ __('Register repository') }}</x-primary-button>
+                            <div class="mx-auto text-center">
+                                <x-primary-button>{{ __('Register repository') }}</x-primary-button>
+                            </div>
                         </form>
                     </div>
                 </div>
