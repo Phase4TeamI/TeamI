4a7f8ab87cb317a20cb596d387d63f649196eb71
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2023-02-27 18:53:28 +0900

    welcomページを調整

--- resources/views/repository/create.blade.php
+++ resources/views/repository/create.blade.php
@@ -31,12 +31,12 @@
                                 <x-text-input name="webhook_secret" type="text" class="mt-1 block w-full"/>
                             </div>
                             <div class="mx-auto text-center">
-                                <a class="mx-auto text-center py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" href="{{ route('repository.create') }}">
+                                <x-primary-button>
                                     <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                         <path d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
                                     </svg>
                                     Register Repository
-                                </a>
+                                </x-primary-button>
                             </div>
                         </form>
                     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,25 @@
 		<title>ProducTrack</title>
 
 		@vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/index.js', 'resources/css/index.css'])
+
+		<style>
+			.backvideo {
+				z-index: -1;
+				position: fixed;
+				width: 100%;
+				top: 0;
+				right: 0;
+				bottom: 0;
+				left: 0;
+			}
+		</style>
 	</head>
 
-  <body class="bg-cover bg-white dark:bg-black">
-  	<div class="h-screen flex justify-center items-center relative overflow-hidden before:absolute before:top-0 before:left-1/2 before:bg-no-repeat before:bg-top before:w-full before:h-full before:-z-[1] before:transform before:-translate-x-1/2 ">
-      <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-20">
-				
+	<body>
+		<video src="{{ asset('images/back_mov.mp4')}}" loop muted autoplay playsinline class="backvideo">
+		</video>
+		<div class="h-screen flex justify-center items-center relative overflow-hidden before:absolute before:top-0 before:left-1/2 before:bg-no-repeat before:bg-top before:w-full before:h-full before:-z-[1] before:transform before:-translate-x-1/2 ">
+			<div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-20 rounded-md backdrop-blur-lg backdrop-blur-sm bg-black/30">
 				<!-- Announcement Banner -->
 				<div class="flex justify-center">
 					<a class="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-xs text-gray-600 p-2 px-3 rounded-full transition hover:border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-400" href="https://github.com/Phase4TeamI/TeamI">
@@ -61,6 +74,6 @@
 				@endif
 				<!-- End Buttons -->
 			</div>
-		</div> 
+		</div>
 	</body>
 </html>
